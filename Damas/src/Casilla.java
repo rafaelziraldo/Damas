88f37cea3897fc093dee43bdce7ac97a9adfbeae
Usuario <Usuario@192.168.1.8>
2017-05-22 21:58:14 -0300

    Mejora

--- Damas/src/Casilla.java
+++ Damas/src/Casilla.java
@@ -8,12 +8,18 @@ public class Casilla {
 	public void recibir(Ficha f){
 		ficha.add(f);
 	}
-	public void Sacar(Ficha f){
+	public void sacar(Ficha f){
 		ficha.clear();
 	}
-	public void PasarA(Casilla otraCasilla,Ficha f){
-		this.Sacar(f);
+	public void pasarA(Casilla otraCasilla,Ficha f){
+		this.sacar(f);
 		otraCasilla.recibir(f);
+	}
+	public void captura(Ficha otraFicha,Casilla casilla,Casilla casillaDestino){
+		if(ficha.get(0).puedeCapturar(otraFicha)&&casilla.tieneFicha(casilla)&& !casillaDestino.tieneFicha(casillaDestino)){
+			this.pasarA(casillaDestino,ficha.get(0));
+		}
+		
 	}
 	public boolean tieneFicha(Casilla casilla){
 		if(ficha.size()==1){
